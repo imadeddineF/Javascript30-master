9595e820ad42e3c61646a234d3a19a2d37d2eeb4
imadeddineF <imadeddinefillalipro@gmail.com>
2024-06-18 01:30:09 +0100

    challenge 1 done

--- 01 - JavaScript Drum Kit/app.js
+++ 01 - JavaScript Drum Kit/app.js
@@ -1,1 +1,14 @@
-console.log("Hello from app.js!");
+const keys = document.querySelectorAll(".key");
+
+keys.forEach((key) => {
+  key.addEventListener("click", (e) => {
+    const dataKey = e.currentTarget.dataset.key;
+
+    const audio = document.querySelector(`audio[data-key="${dataKey}"]`);
+
+    if (audio) {
+      audio.currentTime = 0;
+      audio.play();
+    }
+  });
+});
